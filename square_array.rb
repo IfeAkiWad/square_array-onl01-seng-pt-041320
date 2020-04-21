505748be517515786ec99ad30e55ae0adeca66a3
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-21 01:04:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,31 @@
 def square_array(array)
-new_array=[]
-  array.each do |num|
-    new_array.push(num*num)
-
-  end
-  new_array
+new_array = []
+  array.each{ |num|
+    new_array = num**
+  }
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def square_array(array)
+# new_array=[]
+#   array.each do |num|
+#     new_array.push(num*num)
+#
+#   end
+#   new_array
+# end
 
 # numbers = [1, 2, 3]
 # numbers.each
